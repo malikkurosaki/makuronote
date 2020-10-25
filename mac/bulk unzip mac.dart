59b6716c450d5291fac00f924545e166afb51c36
malik kurosaki <kurosakiblackangel@gmail.com>
2020-10-25 17:24:47 +0800

    Create bulk unzip mac.dart

---
+++ mac/bulk unzip mac.dart
@@ -0,0 +1,12 @@
+# bulk unzip mac 
+
+```bash
+# go to the containing folder
+cd /Users/phil/Downloads/folder_with_zips
+
+# backslash with SHIFT+ALT+7
+unzip \*.zip
+
+# cleanup unwanted zips
+rm -f *.zip
+```
