9d112d15a8f993ae7ad321cb93d5522fe222a862
malik kurosaki <kurosakiblackangel@gmail.com>
2020-10-25 09:07:17 +0800

    Create flutter extention atau perpanjangan.dart

---
+++ flutter/flutter extention atau perpanjangan.dart
@@ -0,0 +1,13 @@
+# flutter extention atau perpanjangan
+
+```dart
+extension on Text{
+  besar(){
+    return Text(this.data,
+      style: TextStyle(
+        color: Colors.red
+      )
+    );
+  }
+}
+```
