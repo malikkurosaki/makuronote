64b206221ebd6f986ba189ac4e23187f95f6a3e4
Amalia <128279129+amaliadwiy@users.noreply.github.com>
2023-12-15 11:50:08 +0800

    Update instalasi.sh
    ok

--- postgres/instalasi.sh
+++ postgres/instalasi.sh
@@ -1,28 +1,28 @@
 brew update
 brew install postgresql
 brew services start postgresql
-
+createuser -s postgres
 psql -U postgres
+
+# membuat user dengan kemampuan membuat database
 CREATE USER nama_pengguna WITH PASSWORD 'kata_sandi';
 ALTER USER nama_pengguna CREATEDB;
 
 psql -U nama_pengguna -d postgres
 CREATE DATABASE nama_database;
 
-
+# ganti port
 brew services stop postgresql
-nano /usr/local/var/postgres/postgresql.conf
+sudo nano /opt/homebrew/var/postgresql@14/postgresql.conf
 port = 5433
 brew services start postgresql
 
-
-nano /usr/local/var/postgres/pg_hba.conf
+# paksa user mengggunakan password
+nano /opt/homebrew/var/postgresql@14/pg_hba.conf
 # "local" is for Unix domain socket connections only
 local   all             all                                     md5
 brew services restart postgresql
 
 
-sudo su - postgres
-psql
 
-sudo -u postgres psql
+
