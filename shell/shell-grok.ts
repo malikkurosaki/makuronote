64653fa4752789a0d9cb074c7ada36a44655730f
bip production <119274726+bipproduction@users.noreply.github.com>
2025-03-17 15:34:59 +0800

    Update shell-grok.ts

--- shell/shell-grok.ts
+++ shell/shell-grok.ts
@@ -1,205 +1,260 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
 
-interface ShellOptions {
-  env: NodeJS.ProcessEnv;
-  cwd: string;
-  throwOnError: boolean;
-}
-
-interface ShellResult {
+interface ShellOutput {
   stdout: Buffer;
   stderr: Buffer;
   exitCode: number;
+  text: (encoding?: BufferEncoding) => string;
+  json: () => any;
+  arrayBuffer: () => ArrayBufferLike;
+  blob: () => Blob;
+  bytes: () => Uint8Array;
 }
 
-class ShellError extends Error {
-  constructor(message: string, public exitCode: number, public stdout: Buffer, public stderr: Buffer) {
+// Definisi class ShellError
+class ShellError extends Error implements ShellOutput {
+  readonly stdout: Buffer;
+  readonly stderr: Buffer;
+  readonly exitCode: number;
+
+  constructor(
+    message: string,
+    stdout: Buffer,
+    stderr: Buffer,
+    exitCode: number
+  ) {
     super(message);
-    this.name = 'ShellError';
+    this.stdout = stdout;
+    this.stderr = stderr;
+    this.exitCode = exitCode;
+  }
+
+  text(encoding: BufferEncoding = "utf8"): string {
+    return this.stdout.toString(encoding);
+  }
+
+  json(): any {
+    return JSON.parse(this.stdout.toString("utf8"));
+  }
+
+  arrayBuffer(): ArrayBufferLike {
+    return this.stdout.buffer.slice(
+      this.stdout.byteOffset,
+      this.stdout.byteOffset + this.stdout.byteLength
+    );
+  }
+
+  blob(): Blob {
+    return new Blob([this.stdout]);
+  }
+
+  bytes(): Uint8Array {
+    return new Uint8Array(this.stdout);
   }
 }
 
-// Interface untuk tipe kembalian $
-interface ShellPromise extends Promise<ShellResult> {
-  quiet(): ShellPromise;
-  nothrow(): ShellPromise;
-  env(newEnv?: Record<string, string>): ShellPromise;
-  cwd(newCwd: string): ShellPromise;
-  text(): Promise<string>;
-  json<T = any>(): Promise<T>;
-  lines(): Promise<string[]>;
-  buffer(): Promise<Buffer>;
-  exec(): Promise<ShellResult>;
-  exitCode(): Promise<number>; // Ditambahkan
+// Definisi class ShellPromise
+class ShellPromise extends Promise<ShellOutput> {
+  private _cwd: string = process.cwd();
+  private _env: NodeJS.ProcessEnv = { ...process.env };
+  private _quiet: boolean = false;
+  private _throwOnError: boolean = true;
+  private _stdin: WritableStream | undefined;
+
+  get stdin(): WritableStream {
+    if (!this._stdin) {
+      this._stdin = new WritableStream();
+    }
+    return this._stdin;
+  }
+
+  cwd(newCwd: string): this {
+    this._cwd = newCwd;
+    return this;
+  }
+
+  env(newEnv: NodeJS.ProcessEnv): this {
+    this._env = newEnv ? { ...newEnv } : ({} as NodeJS.ProcessEnv);
+    return this;
+  }
+
+  quiet(): this {
+    this._quiet = true;
+    return this;
+  }
+
+  async *lines(): AsyncIterable<string> {
+    this.quiet();
+    const output = await this;
+    const text = output.text();
+    for (const line of text.split("\n")) {
+      if (line) yield line;
+    }
+  }
+
+  text(encoding: BufferEncoding = "utf8"): Promise<string> {
+    this.quiet();
+    return this.then((output) => output.text(encoding));
+  }
+
+  json(): Promise<any> {
+    this.quiet();
+    return this.then((output) => output.json());
+  }
+
+  arrayBuffer(): Promise<ArrayBufferLike> {
+    this.quiet();
+    return this.then((output) => output.arrayBuffer());
+  }
+
+  blob(): Promise<Blob> {
+    this.quiet();
+    return this.then((output) => output.blob());
+  }
+
+  nothrow(): this {
+    this._throwOnError = false;
+    return this;
+  }
+
+  throws(shouldThrow: boolean): this {
+    this._throwOnError = shouldThrow;
+    return this;
+  }
 }
 
-/**
- * ShellCommand class untuk menangani eksekusi command
- */
-class ShellCommand {
-  private options: ShellOptions;
-  private isQuiet: boolean = false;
-  private resultPromise: Promise<ShellResult>;
+// Definisi interface untuk Shell
+interface Shell {
+  $: (strings: TemplateStringsArray, ...expressions: any[]) => ShellPromise;
+  braces: (pattern: string) => string[];
+  escape: (input: string) => string;
+  env: (newEnv?: NodeJS.ProcessEnv) => Shell;
+  cwd: (newCwd?: string) => Shell;
+  nothrow: () => Shell;
+  throws: (shouldThrow: boolean) => Shell;
+  readonly ShellError: typeof ShellError;
+  readonly ShellPromise: typeof ShellPromise;
+}
 
-  constructor(defaultOptions: ShellOptions, strings: TemplateStringsArray, values: any[]) {
-    this.options = { ...defaultOptions };
-    this.resultPromise = this.execute(strings, values);
-  }
+// Implementasi Shell
+class ShellImpl implements Shell {
+  private _defaultCwd: string = process.cwd();
+  private _defaultEnv: NodeJS.ProcessEnv = { ...process.env };
+  private _defaultThrowOnError: boolean = true;
 
-  private buildCommand(strings: TemplateStringsArray, values: any[]): string {
-    let command = '';
-    for (let i = 0; i < strings.length; i++) {
-      command += strings[i];
-      if (i < values.length) {
-        command += this.escapeValue(values[i]);
-      }
-    }
-    return command;
-  }
+  readonly ShellError = ShellError;
+  readonly ShellPromise = ShellPromise;
 
-  private escapeValue(value: any): string {
-    if (typeof value === 'string') {
-      return `"${value.replace(/"/g, '\\"')}"`;
-    }
-    return String(value);
-  }
+  $(strings: TemplateStringsArray, ...expressions: any[]): ShellPromise {
+    const command = this.buildCommand(strings, expressions);
 
-  private execute(strings: TemplateStringsArray, values: any[]): Promise<ShellResult> {
-    const command = this.buildCommand(strings, values);
-    return new Promise((resolve, reject) => {
-      const [cmd, ...args] = command.split(' ');
-      const proc: ChildProcessWithoutNullStreams = spawn(cmd, args, {
-        env: this.options.env,
-        cwd: this.options.cwd,
-        shell: true
-      });
+    return new ShellPromise(async (resolve, reject) => {
+      try {
+        const { stdout, stderr, exitCode } = await this.executeCommand(command);
 
-      let stdout = '';
-      let stderr = '';
-
-      proc.stdout.on('data', (data: Buffer) => {
-        stdout += data;
-        if (!this.isQuiet) process.stdout.write(data);
-      });
-
-      proc.stderr.on('data', (data: Buffer) => {
-        stderr += data;
-        if (!this.isQuiet) process.stderr.write(data);
-      });
-
-      proc.on('close', (exitCode: number | null) => {
-        const result: ShellResult = {
-          stdout: Buffer.from(stdout),
-          stderr: Buffer.from(stderr),
-          exitCode: exitCode ?? 1
+        const output: ShellOutput = {
+          stdout,
+          stderr,
+          exitCode,
+          text: (encoding = "utf8") => stdout.toString(encoding),
+          json: () => JSON.parse(stdout.toString("utf8")),
+          arrayBuffer: () =>
+            stdout.buffer.slice(
+              stdout.byteOffset,
+              stdout.byteOffset + stdout.byteLength
+            ),
+          bytes: () => new Uint8Array(stdout),
+          blob: () => new Blob([stdout]),
         };
 
-        if (exitCode !== 0 && this.options.throwOnError) {
-          reject(new ShellError(
+        if (exitCode !== 0 && this._defaultThrowOnError) {
+          throw new ShellError(
             `Command failed with exit code ${exitCode}`,
-            exitCode ?? 1,
-            result.stdout,
-            result.stderr
-          ));
-        } else {
-          resolve(result);
+            stdout,
+            stderr,
+            exitCode
+          );
         }
-      });
+
+        resolve(output);
+      } catch (error) {
+        reject(error);
+      }
+    })
+      .env(this._defaultEnv)
+      .cwd(this._defaultCwd);
+  }
+
+  private buildCommand(
+    strings: TemplateStringsArray,
+    expressions: any[]
+  ): string {
+    return strings.reduce((acc, str, i) => {
+      const expr = expressions[i] !== undefined ? String(expressions[i]) : "";
+      return acc + str + expr;
+    }, "");
+  }
+
+  private async executeCommand(command: string): Promise<{
+    stdout: Buffer;
+    stderr: Buffer;
+    exitCode: number;
+  }> {
+    const { exec } = await import("child_process");
+    return new Promise((resolve) => {
+      exec(
+        command,
+        {
+          cwd: this._defaultCwd,
+          env: this._defaultEnv,
+        },
+        (error, stdout, stderr) => {
+          if (error) {
+            resolve({
+              stdout: Buffer.from(stdout),
+              stderr: Buffer.from(stderr),
+              exitCode: error.code || 1,
+            });
+          } else {
+            resolve({
+              stdout: Buffer.from(stdout),
+              stderr: Buffer.from(stderr),
+              exitCode: 0,
+            });
+          }
+        }
+      );
     });
   }
 
-  // Method untuk membuat ShellPromise dengan modifikasi
-  private createShellPromise(): ShellPromise {
-    const promise = this.resultPromise as ShellPromise;
-    
-    promise.quiet = () => {
-      this.isQuiet = true;
-      return this.createShellPromise();
-    };
-
-    promise.nothrow = () => {
-      this.options.throwOnError = false;
-      return this.createShellPromise();
-    };
-
-    promise.env = (newEnv?: Record<string, string>) => {
-      this.options.env = { ...this.options.env, ...newEnv };
-      return this.createShellPromise();
-    };
-
-    promise.cwd = (newCwd: string) => {
-      this.options.cwd = newCwd;
-      return this.createShellPromise();
-    };
-
-    promise.text = () => {
-      this.isQuiet = true;
-      return this.resultPromise.then(result => result.stdout.toString());
-    };
-
-    promise.json = <T = any>() => {
-      this.isQuiet = true;
-      return this.resultPromise.then(result => JSON.parse(result.stdout.toString()) as T);
-    };
-
-    promise.lines = () => {
-      this.isQuiet = true;
-      return this.resultPromise.then(result => 
-        result.stdout.toString().split('\n').filter(line => line.length > 0)
-      );
-    };
-
-    promise.buffer = () => {
-      this.isQuiet = true;
-      return this.resultPromise.then(result => result.stdout);
-    };
-
-    promise.exec = () => this.resultPromise;
-
-    promise.exitCode = () => { // Ditambahkan
-      return this.resultPromise.then(result => result.exitCode);
-    };
-
-    return promise;
+  braces(pattern: string): string[] {
+    return [pattern]; // Implementasi sederhana
   }
 
-  // Method publik untuk mendapatkan ShellPromise
-  getShellPromise(): ShellPromise {
-    return this.createShellPromise();
+  escape(input: string): string {
+    return input.replace(/([\\$`"])/g, "\\$1");
+  }
+
+  env(newEnv?: NodeJS.ProcessEnv): this {
+    this._defaultEnv = newEnv ? { ...newEnv } : { ...process.env };
+    return this;
+  }
+
+  cwd(newCwd?: string): this {
+    this._defaultCwd = newCwd || process.cwd();
+    return this;
+  }
+
+  nothrow(): this {
+    this._defaultThrowOnError = false;
+    return this;
+  }
+
+  throws(shouldThrow: boolean): this {
+    this._defaultThrowOnError = shouldThrow;
+    return this;
   }
 }
 
-// Konfigurasi default global
-const defaultOptions: ShellOptions = {
-  env: process.env,
-  cwd: process.cwd(),
-  throwOnError: true
-};
-
-// Fungsi $ sebagai tagged template
-export const $ = Object.assign(
-  (strings: TemplateStringsArray, ...values: any[]): ShellPromise => {
-    const command = new ShellCommand(defaultOptions, strings, values);
-    return command.getShellPromise();
-  },
-  {
-    env(newEnv?: Record<string, string>): void {
-      if (newEnv === undefined) {
-        defaultOptions.env = process.env;
-      } else {
-        defaultOptions.env = { ...process.env, ...newEnv };
-      }
-    },
-    cwd(newCwd?: string): void {
-      defaultOptions.cwd = newCwd || process.cwd();
-    },
-    throws(value: boolean): void {
-      defaultOptions.throwOnError = value;
-    },
-    nothrow(): void {
-      defaultOptions.throwOnError = false;
-    }
-  }
-);
-
+const $ = new ShellImpl();
+export { $ };
